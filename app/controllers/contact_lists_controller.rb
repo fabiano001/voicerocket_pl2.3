4f0fb09eefa07dae17b6a88ec5ba833490b41172
Prelang Builder <builder@prelang.com>
2016-01-20 23:48:09 +0000

    Added require_user_signed_in filters to scaffolded controller

--- app/controllers/contact_lists_controller.rb
+++ app/controllers/contact_lists_controller.rb
@@ -1,4 +1,8 @@
 class ContactListsController < ApplicationController
+
+  #->Prelang (scaffolding:rails/scope_to_user)
+  before_filter :require_user_signed_in, only: [:new, :edit, :create, :update, :destroy]
+
   before_action :set_contact_list, only: [:show, :edit, :update, :destroy]
 
   # GET /contact_lists
